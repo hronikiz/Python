eefdec3305cbe977e7daa4648e1b1d8e8a46bd98
hronikiz <dolbaniob@gmail.com>
2025-02-26 11:45:37 +0200

    Update main.py

--- lab1/main.py
+++ lab1/main.py
@@ -1,9 +1,9 @@
-x = input("Enter your name:")
-print("Hello " + x)  #This is a comment
+
+user_name = input("Enter your name:")
+print("Hello " + user_name)  #This is a comment
 
 float_number = 203021.123
-intNumber = 18
-user_name = "Name"
+age = 18
 UserDescription = """
 lorem ipsum dolor sit amet, 
 consectetur adipiscing elit,
@@ -11,20 +11,35 @@ sed do eiusmod tempor incididunt ut
 labore et dolore magna aliqua.
 """
 
-print(type(UserDescription, user_name))
+print("Type of variable User_Description",type(UserDescription))
+print("Type of variable user_name: ",type(user_name))
+print("Type of variable age: ",type(age))
+print("Type of variable float_number: ",type(float_number))
 
-print(len(user_name))
+print("Length of stroke: ", len(user_name))
 
-txt = user_name.upper()
-print(txt)
+user_name_upper = user_name.upper()
+print("Text in uppercase: ",user_name_upper)
 
-print(UserDescription[0:18])
+substring = UserDescription[5 : 15]
+print("Substring: ", substring)
 
-f_user_name = f"Your name is: {x}"
-print(f_user_name)
+message1 = f"Your name is: {user_name}"
+print(message1)
 
-format_user_name = "Your name is: {name}"
-print(format_user_name.format(name = x))
+message2 = "My name is {}. I am {} years old.".format(user_name, age)
+print(message2)
 
 
 
+txt = "More results from text..."
+substr = txt[4:12]
+print(substr)
+print(substr.strip(""))
+
+txt = "More results from text..."
+print(txt.split("e"))
+
+age = 36
+txt = "My name is Mary, and I am {}"
+print(txt.format(age))
