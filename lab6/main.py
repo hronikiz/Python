9d9aea038426045cbe82e197f1786cfbddb24070
hronikiz <dolbaniob@gmail.com>
2025-04-30 04:43:55 +0300

    Update main.py

--- lab6/main.py
+++ lab6/main.py
@@ -1,13 +1,13 @@
 import re
 
-def is_valid_phone(number):
+def valid(number):
     pattern = r'^(\+373|00373|0)?\d{8}$'
     return re.fullmatch(pattern, number) is not None
 
 while True:
     try:
         phone = input("Введите номер телефона Молдовы: ")
-        if is_valid_phone(phone):
+        if valid(phone):
             print(f"Номер принят: {phone}")
             break
         else:
